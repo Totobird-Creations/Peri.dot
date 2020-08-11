1261a29787a378a1982adbefa3ac6d3b0332dcba
Totobird <>
2020-08-11 12:26:54 -0400

    Fixed broken imports

---
+++ peri.dot/version/context.py
@@ -0,0 +1,9 @@
+##########################################
+# CONTEXT                                #
+##########################################
+
+class Context():
+    def __init__(self, display, parent=None, parententry=None):
+        self.display     = display
+        self.parent      = parent
+        self.parententry = parententry

798311b5c7758535827d2e04069349362bcc2dd4
Snowkeep <craig@snowkeep.ca>
2020-08-12 15:12:34 -0400

    more numeric comparison tests

--- tests/test_types_misc.py
+++ tests/test_types_misc.py
@@ -1,11 +1,26 @@
 import pytest
-from peridot.version.types import NullType
+from peridot.version.types import NullType,StringType
 from peridot.version.exceptions import *
 
 
 class TestPeridotMiscTypes:
     def test_null(self):
         a = NullType()
-        assert(isinstance(a, NullType))
         assert(a.value == None)
         assert(a.type == "Null")
+
+class TestPeridotString:
+    def test_string(self):
+        a = StringType("totobird")
+        assert(isinstance(a.value, str))
+        assert(a.value == "totobird")
+        assert(a.type == "Str")
+
+    def test_string_add(self):
+        a = StringType("totobird")
+        b = StringType(" made this")
+        c,err = a.add(b)
+        assert(isinstance(c.value, str))
+        assert(c.value == "totobird made this")
+        assert(c.type == "Str")
+        assert(err is None)

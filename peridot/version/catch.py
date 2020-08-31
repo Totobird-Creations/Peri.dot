42de88281eafc89be1f560190b2de697297993e2
Totobird <>
2020-08-31 16:02:15 -0400

    Modified README.md

--- peridot/version/catch.py
+++ peridot/version/catch.py
@@ -1,6 +1,8 @@
 import os as _os
 from traceback import format_exc as _format_exc
 from colorama import init as _init, Fore as _Fore, Style as _Style
+import webbrowser
+from getpass import getuser
 _init()
 
 class InternalPeridotError(BaseException): pass
@@ -16,7 +18,8 @@ def catch(func):
 
     except Exception as e:
         size = _os.get_terminal_size()
-        traceback = _format_exc()
+        format_exc = _format_exc()
+        traceback = format_exc
         print(f'{_Style.RESET_ALL}{_Fore.RED}{_Style.BRIGHT}{_HEADER}{"-" * max([size[0] - len(_HEADER) + 1, 0])}{_Style.RESET_ALL}\n')
 
         traceback = traceback.rstrip('\n').split('\n\n')
@@ -62,11 +65,16 @@ def catch(func):
         print(f'{_FOOTER}')
         print(f'{_Style.RESET_ALL}{_Fore.RED}{_Style.BRIGHT}{"-" * size[0]}{_Style.RESET_ALL}')
 
+        format_exc = '```%0a' + format_exc.replace(' ', '%20').replace('\n', '%0a').replace(getuser(), 'USERNAME').replace(getuser(), 'USERNAME') + '%0a```'
+        title = str(e).replace(' ', '%20').replace('\n', '%0a').replace(getuser(), 'USERNAME')
+        webbrowser.open(f'https://github.com/toto-bird/Peri.dot/issues/new?body={format_exc}&title={title}&labels=Critical,Auto%20Generated%20Issue')
+
 
 
     except InternalPeridotError as e:
         size = _os.get_terminal_size()
-        traceback = _format_exc()
+        format_exc = _format_exc()
+        traceback = format_exc
         print(f'{_Style.RESET_ALL}{_Fore.RED}{_Style.BRIGHT}{_HEADER}{"-" * max([size[0] - len(_HEADER) + 1, 0])}{_Style.RESET_ALL}')
 
         traceback = traceback.rstrip('\n').split('\n\n')
@@ -111,3 +119,7 @@ def catch(func):
         print(f'{_Style.RESET_ALL}{_Fore.RED}{_Style.BRIGHT}{"-" * size[0]}{_Style.RESET_ALL}')
         print(f'{_FOOTER}')
         print(f'{_Style.RESET_ALL}{_Fore.RED}{_Style.BRIGHT}{"-" * size[0]}{_Style.RESET_ALL}')
+
+        format_exc = '```%0a' + format_exc.replace(' ', '%20').replace('\n', '%0a').replace(getuser(), 'USERNAME').replace(getuser(), 'USERNAME') + '%0a```'
+        title = str(e).replace(' ', '%20').replace('\n', '%0a').replace(getuser(), 'USERNAME')
+        webbrowser.open(f'https://github.com/toto-bird/Peri.dot/issues/new?body={format_exc}&title={title}&labels=Critical,Auto%20Generated%20Issue')
